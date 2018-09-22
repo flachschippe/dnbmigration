b077e968eead96f471724bd0e9dedb92905a47b5
flachschippe <flachschippe@web.de>
2018-09-22 10:25:30 +0200

    .

--- src/main/java/de/schulzt/dnbmigration/entities/BookRepository.java
+++ src/main/java/de/schulzt/dnbmigration/entities/BookRepository.java
@@ -1,9 +1,11 @@
 package de.schulzt.dnbmigration.entities;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.domain.*;
 import org.springframework.data.repository.*;
+
 
 public interface BookRepository extends CrudRepository<Book, UUID>{
 	
@@ -11,5 +13,5 @@ public interface BookRepository extends CrudRepository<Book, UUID>{
 	Page<Book> findAll(Pageable pageable);
 
 	Book findByTitleIgnoringCase(String title);	
-
+	//List<Book> findByTitleOrAuthorsIgnoringCase(String title);
 }
